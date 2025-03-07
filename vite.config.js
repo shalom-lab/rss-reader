0fdeca6e0d6ce99d2b5bdc2596e5d6674e22d0c1
Shalom <15121177287@163.com>
2025-03-07 20:13:06 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   base: '/rss-reader/', // 替换为你的GitHub仓库名
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  }
 }) 
